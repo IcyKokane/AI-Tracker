6f050ce701080590bb946e9ad9a0fb8790c3183b
IcyKokane <tomjunker45@gmail.com>
2026-08-14 15:40:10 -0700

    Use stable Android 16 SDK for first test build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,12 +2,12 @@ plugins { id("com.android.application") }
 
 android {
     namespace = "com.thefoolish.activityai"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.thefoolish.activityai"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 2
         versionName = "0.1.0-m1-firsttest"
     }
